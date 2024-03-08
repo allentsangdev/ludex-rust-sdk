89c0697138c1bcb614d4fa5188d45ab7102194f8
allentsangdev <allentsang04@gmail.com>
2024-03-08 18:08:20 -0500

    add patch and delete request

--- src/api_client.rs
+++ src/api_client.rs
@@ -111,4 +111,73 @@ impl<'a> ApiClient<'a> {
             }
         }
     }
+
+    pub async fn issue_patch_request<T>(&self, url: &str, body: Body) -> Result<T, StatusCode>
+    where
+        // to make sure the response from the api call is a JSON deserializable and can be fully owned
+        T: DeserializeOwned,
+    {
+        let full_url: String = format!("{}{}", &self.base_path, &url);
+
+        let response: Result<Response, Error> =
+            self.http_client.patch(&full_url).body(body).send().await;
+
+        match &response {
+            Ok(r) => {
+                if r.status() != StatusCode::OK {
+                    // returns a Result enum variant Err
+                    return Err(r.status());
+                }
+            }
+            Err(e) => {
+                println!("{}", e.to_string());
+                return Err(e.status().unwrap());
+            }
+        }
+
+        // Parse the response body as Json
+        let content: Result<T, Error> = response.unwrap().json::<T>().await;
+
+        match content {
+            Ok(response) => Ok(response),
+            Err(e) => {
+                println!("{}", e.to_string());
+                Err(e.status().unwrap())
+            }
+        }
+    }
+
+    pub async fn issue_delete_request<T>(&self, url: &str) -> Result<T, StatusCode>
+    where
+        // to make sure the response from the api call is a JSON deserializable and can be fully owned
+        T: DeserializeOwned,
+    {
+        let full_url: String = format!("{}{}", &self.base_path, &url);
+
+        let response: Result<Response, Error> = self.http_client.delete(&full_url).send().await;
+
+        match &response {
+            Ok(r) => {
+                if r.status() != StatusCode::OK {
+                    // returns a Result enum variant Err
+                    return Err(r.status());
+                }
+            }
+            Err(e) => {
+                println!("{}", e.to_string());
+                return Err(e.status().unwrap());
+            }
+        }
+
+        // Parse the response body as Json
+        let content: Result<T, Error> = response.unwrap().json::<T>().await;
+
+        match content {
+            Ok(response) => Ok(response),
+            Err(e) => {
+                println!("{}", e.to_string());
+                Err(e.status().unwrap())
+            }
+        }
+    }
 }
