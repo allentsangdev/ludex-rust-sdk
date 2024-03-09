30bf74b168a6162a62df5cc8289c2ea4b0dbe5b7
allentsangdev <allentsang04@gmail.com>
2024-03-08 23:12:34 -0500

    add integration test

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-// Import the ApiClient
-// use api_client::ApiClient;
-use ludex_rust_sdk::api_client::ApiClient;
-use ludex_rust_sdk::payout::PayoutListResponse;
-use reqwest::StatusCode;
+// // Import the ApiClient
+// // use api_client::ApiClient;
+// use ludex_rust_sdk::api_client::ApiClient;
+// use ludex_rust_sdk::payout::PayoutListResponse;
+// use reqwest::StatusCode;
 
-#[tokio::main]
-async fn main() {
-    let org_api_key = "b91326ef-a39d-49c3-bf7d-3a6eec416294";
-    let client = ApiClient::new(org_api_key);
+// #[tokio::main]
+// async fn main() {
+//     let org_api_key = "b91326ef-a39d-49c3-bf7d-3a6eec416294";
+//     let client = ApiClient::new(org_api_key);
 
-    // Test for payout mod
-    let payouts: Result<PayoutListResponse, StatusCode> = client.issue_get_request("/payout").await;
-    assert!(payouts.is_ok()); // Check if the result is Ok
-}
+//     // Test for payout mod
+//     let payouts: Result<PayoutListResponse, StatusCode> = client.issue_get_request("/payout").await;
+//     assert!(payouts.is_ok()); // Check if the result is Ok
+// }
+fn main() {}
