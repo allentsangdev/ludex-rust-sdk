91cd4bc6d53b7e33da7f7c33fa1f1eb339fb1914
allentsangdev <allentsang04@gmail.com>
2024-03-08 23:12:50 -0500

    add integration test

---
+++ tests/integration_test_payout.rs
@@ -0,0 +1,24 @@
+use ludex_rust_sdk::client::Client;
+use ludex_rust_sdk::OrganizationScoped;
+use once_cell::sync::Lazy;
+
+// Define a lazy static variable to hold the OrganizationScoped instance
+static LUDEX_ORG_SCOPED: Lazy<OrganizationScoped> = Lazy::new(|| {
+    let api_key = "b91326ef-a39d-49c3-bf7d-3a6eec416294".to_string();
+    OrganizationScoped::new(api_key)
+});
+
+#[tokio::test]
+async fn get_clients() {
+    
+    let client_scoped: &Client = &LUDEX_ORG_SCOPED.client;
+    let response = client_scoped.get_clients().await; // Await the asynchronous response
+    
+    // run cargo test -- --nocapture to print during Rust tests
+    match &response {
+        Ok(r) => println!("{:?}",r),
+        Err(e) => println!("{}", e)
+    }
+
+    assert!(response.is_ok()); 
+}
