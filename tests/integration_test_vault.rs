0535367a787551318accd97990f3ecd4a43a7bb4
allentsangdev <allentsang04@gmail.com>
2024-03-17 14:58:47 -0400

    get rid of vault test warnings

--- tests/integration_test_vault.rs
+++ tests/integration_test_vault.rs
@@ -1,10 +1,10 @@
-use ludex_rust_sdk::vault::Vault;
-use ludex_rust_sdk::ClientScoped;
-use once_cell::sync::Lazy;
-use std::env;
+// use ludex_rust_sdk::vault::Vault;
+// use ludex_rust_sdk::ClientScoped;
+// use once_cell::sync::Lazy;
+// use std::env;
 
-// Define a lazy static variable to hold the OrganizationScoped instance
-static LUDEX_CLIENT_SCOPED: Lazy<ClientScoped> = Lazy::new(|| {
-    let api_key = env::var("LUDEX_CLIENT_API_KEY").expect("LUDEX_CLIENT_API_KEY environment variable not set");
-    ClientScoped::new(api_key)
-});
+// // Define a lazy static variable to hold the OrganizationScoped instance
+// static LUDEX_CLIENT_SCOPED: Lazy<ClientScoped> = Lazy::new(|| {
+//     let api_key = env::var("LUDEX_CLIENT_API_KEY").expect("LUDEX_CLIENT_API_KEY environment variable not set");
+//     ClientScoped::new(api_key)
+// });
